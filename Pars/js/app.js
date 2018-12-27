58dee71a761c2d3dcb4ce3ff514167a044f7a8f5
udacity-sq <saadshaheen121@yahoo.com>
2018-12-27 18:14:02 -0500

    1st commit

---
+++ Pars/js/app.js
@@ -0,0 +1,27 @@
+$(function(){
+	$('#animateBtn').click(function(){
+		animate('#anim-head', 'bounce');
+		animate('#anim-body', 'rubberBand');
+
+		return false;
+	});
+
+	$('#animateBtn2').click(function(){
+		animate('header', 'slideOutUp');
+		setTimeout(function(){
+			$('header').css('visibility', 'hidden');
+
+		}, 1000);
+		
+		return false;
+	});
+
+	//animate
+	function animate(element, animation){
+		$(element).addClass('animated '+animation);
+		var wait = setTimeout(function(){
+			$(element).removeClass('animated '+animation);
+		}, 1000);
+
+	}
+})
